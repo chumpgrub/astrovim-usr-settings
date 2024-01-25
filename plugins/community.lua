1c55f2e220cbbb0d624135ab200af7bc86293581
Mark Furrow <chumpgrub@gmail.com>
2024-01-24 22:18:43 -0800

    Adds Nightfox, Zen Mode and MD Preview

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
 }
